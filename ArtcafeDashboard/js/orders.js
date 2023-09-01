a1783de2a6d13685392fad47cb5625abef78a40b
Andrea Ella Remoreras <remoreras.ae@gmail.com>
2023-09-02 03:44:22 +0800

    added files for dashboard

---
+++ ArtcafeDashboard/js/orders.js
@@ -0,0 +1,39 @@
+const Orders = [
+    {
+        productName: 'WACOM drawing tablet',
+        productNumber: '746362',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Copic Marker',
+        productNumber: '562632',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Sketchbook',
+        productNumber: '712383',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Watercolor Brush',
+        productNumber: '232123',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Palette',
+        productNumber: '337218',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Acrylic Canvas',
+        productNumber: '372732',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+]
